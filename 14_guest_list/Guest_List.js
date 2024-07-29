206c569682328785ab4ddf0c4759ae287a05f38c
LoyalSaadKhan <160644562+LoyalSaadKhan@users.noreply.github.com>
2024-07-29 23:24:14 +0500

    Add files via upload

---
+++ 14_guest_list/Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = [`Saad Khan`, `Hamid Ali`, `Shakeel Ahmed`];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!`);
+}
---
+++ 14_guest_list/Guest_List.ts
@@ -0,0 +1,4 @@
+let Guest_List :string[] = [`Saad Khan`, `Hamid Ali`, `Shakeel Ahmed`];
+for(let i=0; i<Guest_List.length; i++){
+    console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!`)
+}
