ad5b0111ec4707c69bb0e99c78b826d97d3090d4
LoyalSaadKhan <160644562+LoyalSaadKhan@users.noreply.github.com>
2024-07-29 23:07:59 +0500

    Add files via upload

---
+++ 06_stripping_names/Stripping_Names.js
@@ -0,0 +1,4 @@
+var personName = "   \n\tWasim Akram\t\n    ";
+console.log(personName);
+var without_whitespace = personName.trim();
+console.log(without_whitespace);
---
+++ 06_stripping_names/Stripping_Names.ts
@@ -0,0 +1,4 @@
+const personName :string = `   \n\tWasim Akram\t\n    `;
+console.log(personName);
+const without_whitespace :string = personName.trim();
+console.log(without_whitespace);
