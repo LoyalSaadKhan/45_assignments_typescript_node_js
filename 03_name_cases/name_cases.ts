4aa3d9685f37a73a700fd85cc29e8cbf778b18cc
Muhammad Saad Khan <160644562+LoyalSaadKhan@users.noreply.github.com>
2024-08-01 00:10:57 +0500

    Add files via upload

---
+++ 03_name_cases/name_cases.ts
@@ -0,0 +1,11 @@
+// Define a string variable called personName with value "Saad Khan"
+let personName = "Saad Khan,";
+
+// Output a greeting with personName converted to lowercase
+console.log("Hello", personName.toLowerCase(), "would you like to learn trading today?");
+
+// Output a greeting with personName converted to uppercase
+console.log("Hello", personName.toUpperCase(), "would you like to learn trading today?");
+
+// Output a greeting with personName where the first letter of each word is capitalized
+console.log("Hello", personName.replace(/\b\w/g, (char) => char.toUpperCase()), "would you like to learn trading language today?");
